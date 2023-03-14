b3eaacea3002ccd7593821e16b51e362a60d6723
Charlie Lu <charlielu05@gmail.com>
2023-03-14 05:09:29 +0000

    adding tests

---
+++ whale_watcher/tests/test_w2.py
@@ -0,0 +1,16 @@
+from w2 import get_image_scan_findings, ImageDetails
+import json
+from test_helpers import _create_image_manifest, _create_mock_repo
+
+
+def test_get_image_scan_findings(ecr_client):
+    mock_repo_name = 'test-repo'
+    mock_image_tag = 'latest'
+    mock_image = _create_mock_repo(ecr_client, mock_repo_name, mock_image_tag)
+    ecr_client.start_image_scan(repositoryName=mock_repo_name, imageId={"imageTag": mock_image_tag})
+
+    # when
+    response = get_image_scan_findings(ecr_client, mock_image)
+
+    # then
+    assert response["repositoryName"] == mock_repo_name

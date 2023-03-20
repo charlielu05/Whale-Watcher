3db60c134cc35b2c52782d4c954391d64621e3e4
Charlie Lu <charlielu05@gmail.com>
2023-03-20 05:39:54 +0000

    wip: adding fast api

--- whale_watcher/src/w2.py
+++ whale_watcher/src/w2.py
@@ -1,20 +1,30 @@
-
+from fastapi import FastAPI
 import boto3
 from typing import List,Callable
 import re 
 from functools import partial
-from dataclasses import dataclass
+from pydantic import BaseModel
 
-@dataclass
-class ImageDetails:
+# regex string
+regex_image_tag = '(?<=:).*'
+regex_image_sha = '(?<=@).*'
+regex_image_repo = '(?<=\/)(.*?)(?=\:)'
+
+app = FastAPI()
+
+class ImageDetail(BaseModel, frozen=True):
     repoName: str
     imageDigest: str
     imageTag: str
 
-@dataclass
-class AppDetails:
-    applicationType: str
-    applicationName: str
+class ResourceDetail(BaseModel, frozen=True):
+    resourceType: str
+    resourceName: str
+    resourceArn: str
+
+class AppDetails(BaseModel, frozen=True):
+    resourceDetail: ResourceDetail
+    imageDetail: ImageDetail
     
 def return_ecr_client(region_name='ap-southeast-2')->boto3.client:
     # boto3 configuration
@@ -31,9 +41,67 @@ def return_lambda_client(region_name='ap-southeast-2')->boto3.client:
 def get_image_repos(ecr_client:boto3.client)->List[str]:
     return ecr_client.describe_repositories().get('repositories')
     
-def get_image_scan_findings(ecr_client:boto3.client, 
-                            image_detail:ImageDetails)->dict:
+def regex_filter(string_chars:str, regex_pattern:str):
+    return re.search(regex_pattern, string_chars).group()
+ 
+def construct_app_details(functions_details:dict, 
+                  tag_regex_function:Callable, 
+                  repo_name_regex_function:Callable,
+                  sha_regex_function:Callable):
+    # given functions code details containing ImageUri and ResolvedImageUri
+    # parse out the repository name, imageTag and imageDigest
+    app_list = []
+    
+    for function_details in functions_details:
+        for key, val in function_details.items():
+            app_list.append(AppDetails(resourceDetail = key,
+                                       imageDetail = ImageDetail(repoName = repo_name_regex_function(val.get('ImageUri')),
+                                                        imageDigest = sha_regex_function(val.get('ResolvedImageUri')),
+                                                        imageTag = tag_regex_function(val.get('ImageUri')) 
+                                                        )
+                                        )
+                            )
+    
+    return app_list
+
+def get_lambda_app_details(lambda_functions:dict)->List[ResourceDetail]:
+    return [ResourceDetail(
+                resourceType = 'lambda',
+                resourceName = function.get('FunctionName'),
+                resourceArn = function.get('FunctionArn')
+                )
+            for function
+            in lambda_functions.get('Functions')]
+    
+def get_lambda_image(lambda_app_details:List[AppDetails])->dict:
+    lambda_client = return_lambda_client()
+    
+    return [{function_detail: lambda_client.get_function(
+                                                    FunctionName = function_detail.resourceName).get('Code')}
+            for function_detail
+            in lambda_app_details]
+
+def get_severity_counts_from_scan(image_scan_findings:dict)->dict:
+    return image_scan_findings.get('imageScanFindings').get('findingSeverityCounts')
+
+def map_severity(image_app:dict):
+    k, v = image_app.items()
+    
+    return {k: get_severity_counts_from_scan(
+                    get_image_scan_findings(
+                        v
+                    )
+    )}
+    
+def get_severity_counts_from_image_details(image_details:List["dict[AppDetails, ResourceDetail]"])->dict:
+    # given the repository_detail, get the severity count for that image
+    return list(map(map_severity, image_details))
+    
+@app.post('/image/scanFinding/')    
+def get_image_scan_findings(image_detail:ResourceDetail)->dict:
     # get the ECR scan result for a single repository
+    ecr_client = return_ecr_client()
+    
     return  ecr_client.describe_image_scan_findings(
         repositoryName = image_detail.repoName,
         imageId = {
@@ -41,91 +109,32 @@ def get_image_scan_findings(ecr_client:boto3.client,
             'imageTag' : image_detail.imageTag
         }
     )
-
-def get_severity_counts_from_scan(image_scan_findings:dict)->dict:
-    return image_scan_findings.get('imageScanFindings').get('findingSeverityCounts')
-
-def get_severity_counts_from_image_details(ecr_client, image_details:ImageDetails)->dict:
-    # given the repository_detail, get the severity count for that image
-    return {image_details.repoName: get_severity_counts_from_scan(get_image_scan_findings(ecr_client, image_details))}
     
-def regex_filter(string_chars:str, regex_pattern:str):
-    return re.search(regex_pattern, string_chars).group()
- 
-def parse_imageid(functions_details:dict, 
-                  tag_regex_function:Callable, 
-                  repo_name_regex_function:Callable,
-                  sha_regex_function:Callable):
-    # given functions code details containing ImageUri and ResolvedImageUri
-    # parse out the repository name, imageTag and imageDigest
-    temp_list = []
+@app.get("/lambda")
+def get_lambda_functions():
+    lambda_client = return_lambda_client()
     
-    for function_details in functions_details:
-        for key, val in function_details.items():
-            temp_list.append({key: 
-                                ImageDetails(repoName = repo_name_regex_function(val.get('ImageUri')),
-                                             imageDigest = sha_regex_function(val.get('ResolvedImageUri')),
-                                             imageTag = tag_regex_function(val.get('ImageUri')) 
-                                             )
-                            })
-    
-    return temp_list
-
-def get_lambda_functions(lambda_client:boto3.client)->dict:
     return lambda_client.list_functions()
 
-def get_list_functions_name(lambda_functions:dict)->List[str]:
-    return [function.get('FunctionName')
-            for function
-            in lambda_functions.get('Functions')]
-    
-def get_lambda_image(lambda_client:boto3.client, 
-                     lambda_functions:List[str])->dict:
-    return [{function_name: lambda_client.get_function(
-                                                    FunctionName = function_name).get('Code')}
-            for function_name
-            in lambda_functions]
-    
+@app.get('/')
+def main():
+    pass
+   
+
 if __name__ == "__main__":
-    ecr_client = return_ecr_client()
-    lambda_client = return_lambda_client()
-    REPOSITORY_NAME = 'bitnami/jupyterhub'
-    IMAGE_TAG = '3.1.1-debian-11-r11'
-    IMAGE_DIGEST = 'sha256:4af0b7dd95312ccf1ab4e50465dcf870c1de9e99ae8ff40026eba2397bd45d9e'
-    TEST_LAMBDA_NAME = 'flying-dogfish-lambda-from-container-image'
-    
-    image_detail = ImageDetails(repoName = REPOSITORY_NAME,
-                                imageTag = IMAGE_TAG,
-                                imageDigest =  IMAGE_DIGEST)
-    # regex string
-    regex_image_tag = '(?<=:).*'
-    regex_image_sha = '(?<=@).*'
-    regex_image_repo = '(?<=\/)(.*?)(?=\:)'
-    
-    image_scan_finding:dict = get_image_scan_findings(ecr_client, 
-                                                      image_detail)
-
-    severity_counts = get_severity_counts_from_scan(image_scan_finding)
-
-    lambda_functions:dict = get_lambda_functions(lambda_client)
-    
-    functions_name:List[str] = get_list_functions_name(lambda_functions)
-    
-    functions_code_details = get_lambda_image(lambda_client, functions_name)
+    lambda_functions = get_lambda_functions()
+    lambda_functions_details: List[AppDetails] = get_lambda_app_details(lambda_functions)
+    functions_code_details = get_lambda_image(lambda_functions_details)
     
     # set regex function partially and pass to parse function
     tag_regex_filter = partial(regex_filter, regex_pattern = regex_image_tag)
     sha_regex_filter = partial(regex_filter, regex_pattern = regex_image_sha)
     repo_name_regex_filter = partial(regex_filter, regex_pattern = regex_image_repo)
     
-    images_digest_and_tag = parse_imageid(functions_code_details, 
+    app_details: List["dict[AppDetails, ResourceDetail]"] = construct_app_details(functions_code_details, 
                                          tag_regex_filter,
                                          repo_name_regex_filter,
                                          sha_regex_filter)
     
-    # severity count from image details
-    # TODO: fix dict value unpacking into the function
-    images_severity_count = [get_severity_counts_from_image_details(ecr_client, list(image.values())[0])
-                             for image
-                             in images_digest_and_tag]
-                             
+    #images_severity_counts = get_severity_counts_from_image_details(images_digest_and_tag)
+    
